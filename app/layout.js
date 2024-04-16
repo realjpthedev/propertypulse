993802ef1437d039d216043905599c7886a581e5
jpucket8 <jp@jpthedev.com>
2024-04-16 17:09:02 -0400

    create hero, footer, & info boxes

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import Navbar from "@/components/Navbar";
 
 import "@/assets/styles/globals.css";
+import Footer from "@/components/Footer";
 
 export const metadata = {
 	title: "PropertyPulse | Find The Perfect Rental",
@@ -14,6 +15,7 @@ const MainLayout = ({ children }) => {
 			<body>
 				<Navbar />
 				<main>{children}</main>
+				<Footer />
 			</body>
 		</html>
 	);
