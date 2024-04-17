38c575f1a2fe9aaae9f9178824ad5d7825015079
jpucket8 <jp@jpthedev.com>
2024-04-17 18:31:06 -0400

    refactor data fetching function to seperate file in utils folder

--- app/properties/page.js
+++ app/properties/page.js
@@ -1,18 +1,5 @@
 import PropertyCard from "@/components/PropertyCard";
-
-async function fetchProperties() {
-	try {
-		const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`);
-
-		if (!res.ok) {
-			throw new Error("Failed to fetch data");
-		}
-
-		return res.json();
-	} catch (error) {
-		console.error(error);
-	}
-}
+import { fetchProperties } from "@/utils/requests";
 
 const PropertiesPage = async () => {
 	const properties = await fetchProperties();
