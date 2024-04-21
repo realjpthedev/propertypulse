cfb12163398d39d9cd6dcb44ad3821ead28677b4
jpucket8 <jp@jpthedev.com>
2024-04-21 19:16:08 -0400

    add toast notifications for deleting a property

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,9 @@ import "@/assets/styles/globals.css";
 import Footer from "@/components/Footer";
 import AuthProvider from "@/components/AuthProvider";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 export const metadata = {
 	title: "PropertyPulse | Find The Perfect Rental",
 	description: "Find your dream rental property",
@@ -18,6 +21,7 @@ const MainLayout = ({ children }) => {
 					<Navbar />
 					<main>{children}</main>
 					<Footer />
+					<ToastContainer />
 				</body>
 			</html>
 		</AuthProvider>
--- app/profile/page.js
+++ app/profile/page.js
@@ -6,6 +6,7 @@ import Link from "next/link";
 import { useSession } from "next-auth/react";
 import profileDefault from "@/assets/images/profile.png";
 import Spinner from "@/components/Spinner";
+import { toast } from "react-toastify";
 
 const ProfilePage = () => {
 	const { data: session } = useSession();
@@ -60,13 +61,13 @@ const ProfilePage = () => {
 
 				setProperties(updatedProperties);
 
-				alert("Property deleted");
+				toast.success("Property deleted");
 			} else {
-				alert("Failed to delete property");
+				toast.error("Failed to delete property");
 			}
 		} catch (error) {
 			console.error(error);
-			alert("Failed to delete property");
+			toast.error("Failed to delete property");
 		}
 	};
 
