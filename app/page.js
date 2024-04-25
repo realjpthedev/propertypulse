ebb4ea46d07c69cba3e0933be03cb210488b6afc
jpucket8 <jp@jpthedev.com>
2024-04-25 17:41:31 -0400

    implement featured properties section

--- app/page.js
+++ app/page.js
@@ -1,12 +1,16 @@
+import FeaturedProperties from "@/components/FeaturedProperties";
 import Hero from "@/components/Hero";
 import HomeProperties from "@/components/HomeProperties";
 import InfoBoxes from "@/components/InfoBoxes";
+import connectDB from "@/config/database";
 
-const HomePage = () => {
+const HomePage = async () => {
+	await connectDB();
 	return (
 		<>
 			<Hero />
 			<InfoBoxes />
+			<FeaturedProperties />
 			<HomeProperties />
 		</>
 	);
