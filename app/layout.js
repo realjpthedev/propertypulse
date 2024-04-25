ea907410664b2e2204865db3a3743a38c224585c
jpucket8 <jp@jpthedev.com>
2024-04-25 16:29:48 -0400

    implement lightbox

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,7 @@ import { GlobalProvider } from "@/context/GlobalContext";
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import "photoswipe/dist/photoswipe.css";
 
 export const metadata = {
 	title: "PropertyPulse | Find The Perfect Rental",
