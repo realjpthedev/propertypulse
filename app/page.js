5580f5345bf98da3c4bb096e300993bf5ff72528
jpucket8 <jp@jpthedev.com>
2024-04-25 22:26:37 -0400

    remove async from homepage component

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Hero from "@/components/Hero";
 import HomeProperties from "@/components/HomeProperties";
 import InfoBoxes from "@/components/InfoBoxes";
 
-const HomePage = async () => {
+const HomePage = () => {
 	return (
 		<>
 			<Hero />
