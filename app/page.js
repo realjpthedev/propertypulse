eccf0b850942fff1650e2d8c0192cabaea6c6efe
jpucket8 <jp@jpthedev.com>
2024-04-25 21:39:37 -0400

    remove connect db from homepage

--- app/page.js
+++ app/page.js
@@ -2,10 +2,8 @@ import FeaturedProperties from "@/components/FeaturedProperties";
 import Hero from "@/components/Hero";
 import HomeProperties from "@/components/HomeProperties";
 import InfoBoxes from "@/components/InfoBoxes";
-import connectDB from "@/config/database";
 
 const HomePage = async () => {
-	await connectDB();
 	return (
 		<>
 			<Hero />
