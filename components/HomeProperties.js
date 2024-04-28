cb14605c4151c0db6c84b387b6416c51f0c73c28
jpucket8 <jp@jpthedev.com>
2024-04-28 16:53:14 -0400

    try to fix bug by removing sort from homeproperties component

--- components/HomeProperties.js
+++ components/HomeProperties.js
@@ -4,9 +4,7 @@ import { fetchProperties } from "@/utils/requests";
 
 const HomeProperties = async () => {
 	const data = await fetchProperties();
-	const recentProperties = data.properties
-		.sort(() => Math.random() - Math.random())
-		.slice(0, 3);
+	const recentProperties = data.properties || [];
 
 	return (
 		<>
